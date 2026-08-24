0c351fbbeee8998a480b65520678eaa2cdbe91a7
Yugo25 <mugiwara_yugo@icloud.com>
2026-08-24 11:27:04 -0400

    Make a color picker

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').testContent = 'Color Code:';
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () => {
+    // Change the background color 
+    document.body.style.backgroundColor = color.value;
+
+    // Show the color code
+    if (color.value === '#ffffff') {
+        text.textContent = `Color Code: ${color.value} (White)`;
+    } else if (color.value === '#000000'){
+        text.textContent = `Color Code: ${color.value} (Black)`;
+    } else {
+        text.textContent = `Color Code: ${color.value}`;
+    }
+}
+
+// Use colorBg when the color picker changes
+color.addEventListener('input', colorBg);
